db3cd9e3cc54d839d896225e88f83014f9191e9c
nan-mu <mu.nan.11@outlook.com>
2024-04-12 22:21:51 +0800

    完成了动态加载的列表

--- pages/choose_lesson.vue
+++ pages/choose_lesson.vue
@@ -1,13 +1,19 @@
 <template>
 
     <!-- <var-col></var-col> -->
-    <var-cell><var-chip block>
-            {{ get_data.课程名称 }}
-        </var-chip></var-cell>
-    <var-cell>
+
+    <var-list :finished="finished" v-model:loading="loading" @load="load">
+        <var-cell>
+            <var-cell :key="item" v-for="item in list">
+                {{ item.课程名称 }}
+            </var-cell>
+        </var-cell>
+    </var-list>
+
+    <!-- <var-cell>
         <var-space>
             <var-chip plain>
-                系: {{ get_data.系 }}
+                系: {{ item.系 }}
             </var-chip>
             <var-chip type="warning">
                 {{ get_data.课程性质 }}
@@ -26,21 +32,39 @@
             </var-chip>
         </var-space>
 
-    </var-cell>
+    </var-cell> -->
 
 </template>
 
 <script setup lang="ts">
 import { onMounted } from "vue";
 import { Snackbar } from '@varlet/ui'
-const get_data = ref("");
-onMounted(async () => {
-    let buffer_lesson_list = await useFetch('/api/choose_lesson');
-    let buffer_lesson_info = await useFetch("/api/get_by_key", {
-        query: { "key": buffer_lesson_list.data.value[1] }
-    });
 
-    let lesson_info = buffer_lesson_info.data.value;
-    get_data.value = lesson_info;
+const list = ref([]);
+const loading = ref(false);
+const finished = ref(false);
+
+let buffer_lesson_list = (await useFetch('/api/choose_lesson')).data.value;
+
+buffer_lesson_list.pop();
+
+onMounted(async () => {
+    for (let index = 0; index < 10; index++) {
+        load();
+    }
 })
+
+const load = async () => {
+    for (let index = 0; index < 10; index++) {
+        let buffer_lesson_info = await useFetch("/api/get_by_key", {
+            query: { "key": buffer_lesson_list.shift() }
+        });
+        list.value.push(buffer_lesson_info.data.value);
+        loading.value = false;
+        if (buffer_lesson_list.length == 0) {
+            finished.value = true;
+            break;
+        }
+    }
+}
 </script>
