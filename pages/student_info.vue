3f46be1468e36449dd9c451f112daadc94c44a59
nan-mu <mu.nan.11@outlook.com>
2024-04-13 23:50:59 +0800

    一种更好的表单

--- pages/student_info.vue
+++ pages/student_info.vue
@@ -1,28 +1,22 @@
 <template>
-    <var-overlay v-model:show="center" :lock-scroll="true" :close-on-key-escape="false" @click="center.value = true">
+    <var-overlay :show="center" :lock-scroll="true" :close-on-key-escape="false">
         <var-paper :elevation="2" :width="350" style="padding: 24px">
             <var-form ref="form" scroll-to-error="start">
-                <var-row>
-                    <var-col :span="23" :offset="1">
-                        <h3>学号</h3>
-                    </var-col>
-                    <var-col :span="23" :offset="1">
-                        <var-input :rules="[v => !!student_id_list.include(v) || '未填写学号或学号错误']" v-model="login.id"
-                            label="学号" placeholder="请输入学号（空着的话会随机登陆一个）" style="width: 100%;" />
-                    </var-col><var-col :span="23" :offset="1"><br /></var-col>
-                    <var-col :span="23" :offset="1">
-                        <var-button block @click="form.validate()">登陆</var-button>
-                    </var-col>
-                </var-row>
+                <var-space direction="column" :size="[20, 0]">
+                    <h3>学号</h3>
+                    <var-input :rules="[veify]" v-model="login.id" label="学号" placeholder="请输入学号（空着的话会随机登陆一个）"
+                        style="width: 100%;" />
+                    <var-button block @click="form.validate()">登陆</var-button>
+                </var-space>
             </var-form>
         </var-paper>
     </var-overlay>
 </template>
 
-<script setup>
+<script setup lang="ts">
 import { onMounted } from "vue";
 // import { Snackbar } from "@varlet/ui";
-
+// student_id_list.include(`+${v}`)
 const active = useState("active_bottom_navigation", () => 2);
 const center = ref(false);
 const form = ref(null);
@@ -32,8 +26,14 @@ const student_id_list = (await useFetch("/api/find", {
     query: { "type": "Student" }
 })).data.value;
 
-function submit(v) {
+function veify(v) {
+    return student_id_list.includes(`+${v}`) || '未填写学号或学号错误'
+}
 
+function submit(v) {
+    if (v) {
+        center.value = false;
+    }
 }
 
 onMounted(() => {
